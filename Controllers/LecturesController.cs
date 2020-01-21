9ff3b7be66273033e058e5f8c4bf9311b5c9f867
Aizad <mimiey970413@gmail.com>
2020-01-21 11:56:12 +0800

    First commet

---
+++ Controllers/LecturesController.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using paSystem.Models;
+
+namespace paSystem.Controllers
+{
+    public class LecturesController : Controller
+    {
+        private myDatabaseEntities db = new myDatabaseEntities();
+
+        // GET: Lectures
+        public ActionResult Index()
+        {
+            return View(db.Lectures.ToList());
+        }
+
+        // GET: Lectures/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lectures/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,lectureName,staffID")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Lectures.Add(lecture);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(lecture);
+        }
+
+        // GET: Lectures/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,lectureName,staffID")] Lecture lecture)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(lecture).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(lecture);
+        }
+
+        // GET: Lectures/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+            return View(lecture);
+        }
+
+        // POST: Lectures/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Lecture lecture = db.Lectures.Find(id);
+            db.Lectures.Remove(lecture);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        // GET: Lectures/Details/5
+
+       
+        public ActionResult StudentList(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Lecture lecture = db.Lectures.Find(id);
+
+
+            List<object> myModel = new List<object>
+            {
+                db.pas.Where(p => p.lectureID == id).ToList(),
+                db.Students.ToList()
+            };
+
+            if (lecture == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(myModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentList([Bind(Include = "Id,lectureID,studentID")] pa pa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pas.Add(pa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pa);
+        }
+
+
+        // GET: Students
+        public ActionResult pasList(int? id)
+        {
+
+            List<object> list = new List<object>();
+            list.Add(db.pas.Where(p => p.lectureID == id).ToList());
+            list.Add(db.Students.ToList());
+
+            return View(list);
+        }
+
+    }
+}
---
+++ Controllers/pasController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using paSystem.Models;
+
+namespace paSystem.Controllers
+{
+    public class pasController : Controller
+    {
+        private myDatabaseEntities db = new myDatabaseEntities();
+
+        // GET: pas
+        public ActionResult Index()
+        {
+            return View(db.pas.ToList());
+        }
+
+        // GET: pas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pa pa = db.pas.Find(id);
+            if (pa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pa);
+        }
+
+        // GET: pas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: pas/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,lectureID,studentID")] pa pa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.pas.Add(pa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pa);
+        }
+
+        // GET: pas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pa pa = db.pas.Find(id);
+            if (pa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pa);
+        }
+
+        // POST: pas/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,lectureID,studentID")] pa pa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pa);
+        }
+
+        // GET: pas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            pa pa = db.pas.Find(id);
+            if (pa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pa);
+        }
+
+        // POST: pas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            pa pa = db.pas.Find(id);
+            db.pas.Remove(pa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
